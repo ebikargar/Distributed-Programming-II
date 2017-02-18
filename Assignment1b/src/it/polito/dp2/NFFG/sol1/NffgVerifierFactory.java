20bc1d4a3d7684194c1af07e88265ccf8d987ea2
EBO-PC\Ebi <ebrahim.kargar@gmail.com>
2017-02-18 02:01:16 +0100

    Assignment 1b has been finished.

---
+++ Assignment1b/src/it/polito/dp2/NFFG/sol1/NffgVerifierFactory.java
@@ -0,0 +1,12 @@
+package it.polito.dp2.NFFG.sol1;
+
+import it.polito.dp2.NFFG.*;
+
+public class NffgVerifierFactory extends it.polito.dp2.NFFG.NffgVerifierFactory {
+
+	// ------------ NffgVerifierFactory -----------------------//
+	@Override
+	public NffgVerifier newNffgVerifier() throws NffgVerifierException {
+		return new MyNffgVerifier();
+	}
+}
